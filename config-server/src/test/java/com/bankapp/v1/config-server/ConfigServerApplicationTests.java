427561d68ce280c6f9efd26d4dc111b7ebdbb6d6
Rounak Das <rounakdas153@gmail.com>
2026-06-18 15:55:47 +0530

    updated the config sanity test

--- config-server/src/test/java/com/bankapp/v1/config-server/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/bankapp/v1/config-server/ConfigServerApplicationTests.java
@@ -1,9 +1,17 @@
 package com.nagarro.bankapp.config_server;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
 
-@SpringBootTest
+//@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.profiles.active=native",
+		"spring.cloud.config.server.native.search-locations=classpath:/config"
+		// ^ points to a mock folder in src/test/resources/config if needed
+})
+@ImportAutoConfiguration(classes = RefreshAutoConfiguration.class)
 class ConfigServerApplicationTests {
 
 	@Test
